4c8632a4a225db9cba78900a54b4a60ab5481df8
Jacques Potgieter <potgieter.g.jacques@gmail.com>
2020-05-27 18:06:38 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -5,17 +5,15 @@ def countdown(number)
     
     puts "#{number} SECOND(S)!"
     number -= 1
-  end
-  'HAPPY NEW YEAR'
+  return 'HAPPY NEW YEAR'
 end
 
 
-def countdown(number)
+def countdown_with_sleep(number)
   while number > 0
     puts "#{number} SECOND(S)!"
     number -= 1
     sleep(1)
-  end
-  'HAPPY NEW YEAR'
+  return 'HAPPY NEW YEAR'
 
 end
